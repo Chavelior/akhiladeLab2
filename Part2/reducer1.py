b52625075e3878b7a21a9fb50d10386255996015
Akhila <akhiladevi95@gmail.com>
2019-04-27 20:03:32 -0400

    First Commit

--- Part2/reducer1.py
+++ Part2/reducer1.py
@@ -5,23 +5,25 @@ from operator import itemgetter
 import sys
 
 def read_mapper_output(file, separator='\t'):
-	for line in file:
-		yield line.rstrip().split(separator, 1)
+    for line in file:
+        yield line.rstrip().split(separator, 1)
 
 def main(separator='\t'):
     # input comes from STDIN (standard input)
-	data = read_mapper_output(sys.stdin, separator=separator)
+    data = read_mapper_output(sys.stdin, separator=separator)
     # groupby groups multiple word-count pairs by word,
     # and creates an iterator that returns consecutive keys and their group:
     #   current_word - string containing a word (the key)
     #   group - iterator yielding all ["&lt;current_word&gt;", "&lt;count&gt;"] items
-	for current_word, group in groupby(data, itemgetter(0)):
-        	try:
-            		total_count = sum(int(count) for current_word, count in group)
-            		print(current_word + separator + str(total_count))
-        	except ValueError:
-            	# count was not a number, so silently discard this item
-            		pass
+    for key, group in groupby(data, itemgetter(0)):
+        try:
+            total_count = sum(int(count) for key, count in group)
+            key = key[1:-2].split(',')
+            print(key[0], key[1], total_count, sep=separator)
+        except ValueError:
+            # count was not a number, so silently discard this item
+            pass
 
 if __name__ == "__main__":
-	main()
+    main()
+
